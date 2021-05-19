d8a29ef8201b1795f16fd28b82760ff7aefb9e4f
Cait McDonald <caitmcdonald@users.noreply.github.com>
2021-05-18 21:20:18 -0600

    ran out of time

--- scratch/orthofinder_test.sh
+++ scratch/orthofinder_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --mem 74GB
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=cait.mcdonald@colostate.edu
 #SBATCH --output=output-%j
